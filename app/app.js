9d861d2e6a2060c93e60cd2b97ada3f5cb2e35e4
Andrew Golightly <andrew@golightlyplus.com>
2015-04-01 00:37:41 +1100

    created a filter that converts decimal numbers to negabinary

--- app/app.js
+++ app/app.js
@@ -1,19 +1,22 @@
 var negabinaryApp = angular.module('negabinaryApp', []);
 
-function decimalToNegabinary(decimal) {
+negabinaryApp.filter('negabinary', function() {
 
-    var negabinary = [];
-    var base = -2;
-    var remainder;
+    return function (decimal) {
 
-    while(decimal != 0) {
+        if (isNaN(decimal)) return "not a number";
+        
+        var negabinary = [];
+        var base = -2;
+        var remainder;
 
-        remainder = decimal % base;
-        decimal = Math.ceil(decimal / base);
-        negabinary.push(remainder >= 0 ? remainder : -remainder);
+        while(decimal != 0) {
+
+            remainder = decimal % base;
+            decimal = Math.ceil(decimal / base);
+            negabinary.push(remainder >= 0 ? remainder : -remainder);
+        }
+
+        return negabinary.reverse().join('');
     }
-
-    return negabinary.reverse().join('');
-}
-
-console.log(decimalToNegabinary(18));
+});
