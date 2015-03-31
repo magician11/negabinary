3f4ebf417ca082d7594119a34ea00be1c13b5505
Andrew Golightly <andrew@golightlyplus.com>
2015-04-01 00:13:04 +1100

    added basic structure and view

---
+++ app/app.js
@@ -0,0 +1,17 @@
+function decimalToNegabinary(decimal) {
+
+    var negabinary = [];
+    var base = -2;
+    var remainder;
+
+    while(decimal != 0) {
+
+        remainder = decimal % base;
+        decimal = Math.ceil(decimal / base);
+        negabinary.push(remainder >= 0 ? remainder : -remainder);
+    }
+
+    return negabinary.reverse().join('');
+}
+
+console.log(decimalToNegabinary(18));
