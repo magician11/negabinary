eef0fe781102e55da774ce83f5005a3520186593
Andrew Golightly <andrew@golightlyplus.com>
2015-04-02 23:33:16 +1100

    displaying calculations for negabinary

--- app/app.js
+++ app/app.js
@@ -1,22 +1,35 @@
-var negabinaryApp = angular.module('negabinaryApp', []);
+var negabinaryApp = angular.module('negabinaryApp', ['ngSanitize']);
 
 negabinaryApp.filter('negabinary', function() {
 
     return function (decimal) {
 
         if (isNaN(decimal)) return "not a number";
-        
+
+        var origDecimal = decimal;
         var negabinary = [];
         var base = -2;
+        var negabinaryCalculation = [];
         var remainder;
+
+        negabinaryCalculation.push('<h4>Calculations</h4><table class="table negabinary-table"><tr><th>Decimal</th><th>Base</th><th>Result</th><th>Remainder</th></tr>');
 
         while(decimal != 0) {
 
+            negabinaryCalculation.push('<tr><td>' + decimal + '</td><td>' + base + '</td><td>');
+
             remainder = decimal % base;
             decimal = Math.ceil(decimal / base);
-            negabinary.push(remainder >= 0 ? remainder : -remainder);
+            negabinaryCalculation.push(decimal + '</td><td>');
+            remainder = remainder >= 0 ? remainder : -remainder;
+            negabinary.push(remainder);
+            negabinaryCalculation.push(remainder + '</td></tr>');
         }
 
-        return negabinary.reverse().join('');
+        negabinaryCalculation.push('</table>');
+
+        negabinaryCalculation.unshift('<h2>' + origDecimal + ' (decimal) = ' + negabinary.reverse().join('') + ' (negabinary)</h2>');
+
+        return negabinaryCalculation.join('');
     }
 });
